c07fe9e291b3bf4a820075822dfb320374c60e4f
hailey <hailey@blueskyweb.xyz>
2026-09-15 06:35:51 +0000

    App: declare ATProtoClient dep (LoginFlow default args reference its types from AppShell objects)

--- App/Package.swift
+++ App/Package.swift
@@ -20,6 +20,7 @@ let package = Package(
     .package(path: "../Packages/UIComponents"),
     .package(path: "../Packages/Features/LoginViews"),
     .package(path: "../Packages/Features/Login"),
+    .package(path: "../Packages/ATProtoClient"),
   ],
   targets: [
     .target(
@@ -30,6 +31,7 @@ let package = Package(
         .product(name: "UIComponents", package: "UIComponents"),
         .product(name: "LoginViews", package: "LoginViews"),
         .product(name: "LoginLogic", package: "Login"),
+        .product(name: "ATProtoClient", package: "ATProtoClient"),
       ],
       path: "Sources/AppShell"
     )
