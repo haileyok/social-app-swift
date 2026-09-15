9a06e36c007e635b089086fa5be5e3e87f147575
hailey <hailey@blueskyweb.xyz>
2026-09-15 16:03:11 +0000

    Fix App/Package.swift conflict markers (postthreadviews merge fallout)

--- App/Package.swift
+++ App/Package.swift
@@ -18,12 +18,9 @@ let package = Package(
     // UI packages (macOS-CI-built). Added as they land:
     .package(path: "../Packages/DesignSystem"),
     .package(path: "../Packages/UIComponents"),
-<<<<<<< HEAD
     .package(path: "../Packages/Features/ProfileViews"),
-=======
     .package(path: "../Packages/Features/PostThreadViews"),
     .package(path: "../Packages/Features/PostThread"),
->>>>>>> 46e56a741e2dc9dd62c6459f30bd0afdd76ea47f
     .package(path: "../Packages/Features/LoginViews"),
     .package(path: "../Packages/Features/Login"),
     .package(path: "../Packages/Features/ComposerViews"),
